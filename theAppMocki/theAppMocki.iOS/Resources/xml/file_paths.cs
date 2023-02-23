17e1c4d2351060ba8b314b22d838522d63e05709
Piotr Andrzejewski <xividi123@gmail.com>
2023-02-23 13:36:24 +0100

    somethings

---
+++ theAppMocki/theAppMocki.iOS/Resources/xml/file_paths.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace theAppMocki.iOS.Resources.xml
+{
+    public class file_paths : ContentPage
+    {
+        public file_paths ()
+        {
+            Content = new StackLayout { 
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
+
